c02afa016d11f0d590d7d9489f6be59032953663
TheMrXGaming <the_mr_x_gaming@hotmail.com>
2026-07-29 22:24:04 +0200

    Update supabase-config.js

--- wetboek/supabase-config.js
+++ wetboek/supabase-config.js
@@ -3,4 +3,4 @@ window.GH_SUPABASE_URL = "https://yyaruoqiadwfwpvqcazy.supabase.co";
 
 // Plak tussen de aanhalingstekens de volledige Publishable key uit:
 // Supabase > Project Settings > API Keys > Publishable key
-window.GH_SUPABASE_PUBLISHABLE_KEY = "PLAK_HIER_DE_VOLLEDIGE_SB_PUBLISHABLE_KEY";
+window.GH_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_jwHVX8hTsQ5z5RkkWqXCQg_ieh_GOaD";
